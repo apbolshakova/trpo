075f1e53039d0304457d0b29f4f11b9c3e03542a
julody <julody@mail.ru>
2021-03-01 05:49:37 +0300

    feat(lw3): implemented Figure and FigureComparer classes

--- trpo-lw3/trpo-lw3/Program.cs
+++ trpo-lw3/trpo-lw3/Program.cs
@@ -6,10 +6,86 @@ using System.Threading.Tasks;
 
 namespace trpo_lw3
 {
+    abstract class Figure
+    {
+        private enum ColorEnum
+        {
+            Red, Green, Blue
+        }
+
+        private string _name;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception("Ошибка: название фигуры не может быть пустым!");
+                }
+
+                _name = value;
+            }
+        }
+
+        private ColorEnum _color;
+
+        public string Color
+        {
+            get => _color.ToString();
+            set
+            {
+                if (Enum.IsDefined(typeof(ColorEnum), value))
+                {
+                    throw new Exception("Ошибка: некорректный цвет! Допустимые цвета: Red, Green или Blue.");
+                }
+
+                _color = (ColorEnum) Enum.Parse(typeof(ColorEnum), value);
+            }
+        }
+
+        public abstract void PrintDimension();
+    }
+
+    class FigureComparer : IComparer<Figure>
+    {
+        public enum CompareField : byte
+        {
+            ByName = 0,
+            ByColor = 1
+        }
+
+        public CompareField Field { get; set; }
+
+        public int Compare(Figure x, Figure y)
+        {
+            switch (Field)
+            {
+                case CompareField.ByName:
+                    return string.Compare(x?.Name, y?.Name, StringComparison.Ordinal);
+                case CompareField.ByColor:
+                    return string.Compare(x?.Color, y?.Color, StringComparison.Ordinal);
+                default:
+                    return 0;
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            PrintVariant();
+            Console.ReadKey();
+        }
+
+        private static void PrintVariant()
+        {
+            const char c1 = 'O';
+            const char c2 = 'N';
+            const int variant = ((int)c1 + (int)c2) % 9;
+            Console.WriteLine($"Вариант {variant}");
         }
     }
 }
