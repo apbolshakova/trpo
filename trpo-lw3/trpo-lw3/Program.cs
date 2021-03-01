d831f4ae4eae4814a0650a818fde221520468cd6
julody <julody@mail.ru>
2021-03-01 06:51:59 +0300

    feat(lw3): implemented shapes classes

--- trpo-lw3/trpo-lw3/Program.cs
+++ trpo-lw3/trpo-lw3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +9,7 @@ namespace trpo_lw3
 {
     abstract class Figure
     {
-        private enum ColorEnum
-        {
-            Red, Green, Blue
-        }
-
         private string _name;
-
         public string Name
         {
             get => _name;
@@ -29,8 +24,11 @@ namespace trpo_lw3
             }
         }
 
+        private enum ColorEnum
+        {
+            Red, Green, Blue
+        }
         private ColorEnum _color;
-
         public string Color
         {
             get => _color.ToString();
@@ -51,6 +49,13 @@ namespace trpo_lw3
         {
             throw new Exception("Ошибка: попытка получить площадь неопределённой фигуры!");
         }
+
+        public virtual void PrintInfo()
+        {
+            Console.WriteLine("Тип: неопределённая фигура");
+            Console.WriteLine($"Название: {Name}");
+            Console.WriteLine($"Цвет: {Color}");
+        }
     }
 
     class FigureComparer : IComparer<Figure>
@@ -60,9 +65,7 @@ namespace trpo_lw3
             ByName = 0,
             ByColor = 1
         }
-
         public CompareField Field { get; set; }
-
         public int Compare(Figure x, Figure y)
         {
             switch (Field)
@@ -77,9 +80,9 @@ namespace trpo_lw3
         }
     }
 
-    class Shape2D : Figure {
+    class Shape2D : Figure 
+    {
         private double[] _centerCoords;
-
         public double[] CenterCoords
         {
             get => _centerCoords;
@@ -96,14 +99,13 @@ namespace trpo_lw3
 
         public sealed override void PrintDimension()
         {
-            Console.WriteLine("2D фигура.");
+            Console.WriteLine("Размерность: 2D фигура");
         }
     }
 
     class Shape3D : Figure
     {
         private double[] _centerCoords;
-
         public double[] CenterCoords
         {
             get => _centerCoords;
@@ -120,7 +122,195 @@ namespace trpo_lw3
 
         public sealed override void PrintDimension()
         {
-            Console.WriteLine("3D фигура.");
+            Console.WriteLine("Размерность: 3D фигура");
+        }
+    }
+
+    sealed class Square : Shape2D 
+    {
+        private double _side;
+        public double Side
+        {
+            get => _side;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Ошибка: сторона квадрата должна быть положительной!");
+                }
+
+                _side = value;
+            }
+        }
+
+        public Square(string name, double side, double[] centerCoords, string color)
+        {
+            Name = name;
+            Side = side;
+            CenterCoords = centerCoords;
+            Color = color;
+        }
+
+        public override double GetArea()
+        {
+            return _side * _side;
+        }
+
+        public override void PrintInfo()
+        {
+            Console.WriteLine("Тип: квадрат");
+            Console.WriteLine($"Название: {Name}");
+            Console.WriteLine($"Цвет: {Color}");
+            PrintDimension();
+            Console.WriteLine($"Сторона: {Side}");
+            Console.WriteLine($"Площадь: {GetArea()}");
+        }
+
+        static Square Create(string name, double side, double[] centerCoords, string color)
+        {
+            Square square = new Square(name, side, centerCoords, color);
+            return square;
+        }
+    }
+
+    sealed class Circle : Shape2D
+    {
+        private double _radius;
+        public double Radius
+        {
+            get => _radius;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Ошибка: радиус круга должен быть положительным!");
+                }
+
+                _radius = value;
+            }
+        }
+
+        public Circle(string name, double radius, double[] centerCoords, string color)
+        {
+            Name = name;
+            Radius = radius;
+            CenterCoords = centerCoords;
+            Color = color;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * _radius * _radius;
+        }
+
+        public override void PrintInfo()
+        {
+            Console.WriteLine("Тип: круг");
+            Console.WriteLine($"Название: {Name}");
+            Console.WriteLine($"Цвет: {Color}");
+            PrintDimension();
+            Console.WriteLine($"Радиус: {Radius}");
+            Console.WriteLine($"Площадь: {GetArea()}");
+        }
+
+        static Circle Create(string name, double radius, double[] centerCoords, string color)
+        {
+            Circle circle = new Circle(name, radius, centerCoords, color);
+            return circle;
+        }
+    }
+
+    sealed class Cube : Shape3D
+    {
+        private double _side;
+        public double Side
+        {
+            get => _side;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Ошибка: сторона куба должна быть положительной!");
+                }
+
+                _side = value;
+            }
+        }
+
+        public Cube(string name, double side, double[] centerCoords, string color)
+        {
+            Name = name;
+            Side = side;
+            CenterCoords = centerCoords;
+            Color = color;
+        }
+
+        public override double GetArea()
+        {
+            return 6 * _side * _side;
+        }
+
+        public override void PrintInfo()
+        {
+            Console.WriteLine("Тип: куб");
+            Console.WriteLine($"Название: {Name}");
+            Console.WriteLine($"Цвет: {Color}");
+            PrintDimension();
+            Console.WriteLine($"Сторона: {Side}");
+            Console.WriteLine($"Площадь: {GetArea()}");
+        }
+
+        static Cube Create(string name, double side, double[] centerCoords, string color)
+        {
+            Cube cube = new Cube(name, side, centerCoords, color);
+            return cube;
+        }
+    }
+
+    sealed class Ball : Shape2D
+    {
+        private double _radius;
+        public double Radius
+        {
+            get => _radius;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Ошибка: радиус шара должен быть положительным!");
+                }
+
+                _radius = value;
+            }
+        }
+
+        public Ball(string name, double radius, double[] centerCoords, string color)
+        {
+            Name = name;
+            Radius = radius;
+            CenterCoords = centerCoords;
+            Color = color;
+        }
+
+        public override double GetArea()
+        {
+            return 4 * Math.PI * _radius * _radius;
+        }
+
+        public override void PrintInfo()
+        {
+            Console.WriteLine("Тип: шар");
+            Console.WriteLine($"Название: {Name}");
+            Console.WriteLine($"Цвет: {Color}");
+            PrintDimension();
+            Console.WriteLine($"Радиус: {Radius}");
+            Console.WriteLine($"Площадь: {GetArea()}");
+        }
+
+        static Ball Create(string name, double radius, double[] centerCoords, string color)
+        {
+            Ball ball = new Ball(name, radius, centerCoords, color);
+            return ball;
         }
     }
 
