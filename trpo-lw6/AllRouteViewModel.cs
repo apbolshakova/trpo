ae389ac050ad5eda80a275a557b23dea9fe7513d
julody <julody@mail.ru>
2021-04-25 16:46:16 +0300

    feat(lw6): implementation of adding and removing

---
+++ trpo-lw6/AllRouteViewModel.cs
@@ -0,0 +1,206 @@
+﻿//using System;
+//using System.Collections.ObjectModel;
+//using System.ComponentModel;
+//using System.Windows.Input;
+//using System.Runtime.CompilerServices;
+
+//namespace trpo_lw6
+//{ 
+//    public class AllRouteViewModel : INotifyPropertyChanged
+//    {
+//        private Route _selectedRoute;
+
+//        public ObservableCollection<Route> Routes { get; set; }
+//        public Route SelectedRoute
+//        {
+//            get => _selectedRoute;
+//            set
+//            {
+//                _selectedRoute = value;
+//                OnPropertyChanged("SelectedRoute");
+//            }
+//        }
+
+//        public AllRouteViewModel()
+//        {
+//            Routes = new ObservableCollection<Route>
+//            {
+//                new Route {
+//                    Name = "Мачаме",
+//                    Description = "Маршрут Мачаме предоставляет живописные виды на гору Меру в Национальном Парке Аруши.",
+//                    Peak = "Пик Шира",
+//                    Height = 3962
+
+//                },
+//                new Route {
+//                    Name = "Умбве",
+//                    Description = "Умбве имеет заслуженную репутацию самого трудного маршрута на горе Килиманджаро.",
+//                    Peak = "Пик Ухуру",
+//                    Height = 5895},
+//                new Route
+//                {
+//                    Name = "Марангу",
+//                    Description = "Маршрут Марангу является самым популярным на Килиманджаро.",
+//                    Peak = "Пик Мавензи",
+//                    Height = 5149
+//                },
+//            };
+//        }
+
+//        public event PropertyChangedEventHandler PropertyChanged;
+//        public void OnPropertyChanged(string prop = "")
+//        {
+//            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+//        }
+
+//        private BaseCommand _addCommand;
+//        public BaseCommand AddCommand
+//        {
+//            get
+//            {
+//                return _addCommand ??
+//                       (_addCommand = new BaseCommand(obj =>
+//                       {
+//                           Route route = new Route();
+//                           Routes.Insert(0, route);
+//                           SelectedRoute = route;
+//                       }));
+//            }
+//        }
+
+//        private BaseCommand _delCommand;
+//        public BaseCommand DelCommand
+//        {
+//            get
+//            {
+//                if (_delCommand != null)
+//                    return _delCommand;
+
+//                Action<object> Execute = o =>
+//                {
+//                    Route b = (Route)o;
+//                    Routes.Remove(b);
+//                };
+//                Func<object, bool> CanExecute = o => Routes.Count > 0;
+//                _delCommand = new BaseCommand(Execute, CanExecute);
+//                return _delCommand;
+//            }
+//        }
+
+//    }
+
+//    public class BaseCommand : ICommand
+//    {
+//        private readonly Action<object> _execute;
+//        private readonly Func<object, bool> _canExecute;
+
+//        public event EventHandler CanExecuteChanged
+//        {
+//            add => CommandManager.RequerySuggested += value;
+//            remove => CommandManager.RequerySuggested -= value;
+//        }
+//        public BaseCommand(Action<object> execute, Func<object, bool> canExecute = null)
+//        {
+//            _execute = execute;
+//            _canExecute = canExecute;
+//        }
+//        public bool CanExecute(object parameter)
+//        {
+//            return _canExecute == null || _canExecute(parameter);
+//        }
+//        public void Execute(object parameter)
+//        {
+//            _execute(parameter);
+//        }
+//    }
+
+//}
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+
+namespace trpo_lw6
+{
+    public class AllRouteViewModel : INotifyPropertyChanged
+    {
+        private Route selectedRoute;
+
+        public ObservableCollection<Route> Routes { get; set; }
+        public Route SelectedRoute
+        {
+            get { return selectedRoute; }
+            set
+            {
+                selectedRoute = value;
+                OnPropertyChanged("SelectedRoute");
+            }
+        }
+
+        public AllRouteViewModel()
+        {
+            Routes = new ObservableCollection<Route>
+            {
+                new Route {
+                    Name = "Мачаме",
+                    Description = "Маршрут Мачаме предоставляет живописные виды на гору Меру в Национальном Парке Аруши.",
+                    Peak = "Пик Шира",
+                    Height = 3962
+
+                },
+                new Route {
+                    Name = "Умбве",
+                    Description = "Умбве имеет заслуженную репутацию самого трудного маршрута на горе Килиманджаро.",
+                    Peak = "Пик Ухуру",
+                    Height = 5895},
+                new Route
+                {
+                    Name = "Марангу",
+                    Description = "Маршрут Марангу является самым популярным на Килиманджаро.",
+                    Peak = "Пик Мавензи",
+                    Height = 5149
+                },
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+        
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                       (addCommand = new RelayCommand(obj =>
+                       {
+                           Route phone = new Route();
+                           Routes.Insert(0, phone);
+                           SelectedRoute = phone;
+                       }));
+            }
+        }
+
+        private RelayCommand deleteCommand;
+        public RelayCommand DeleteCommand
+        {
+            get
+            {
+                return deleteCommand ??
+                       (deleteCommand = new RelayCommand(obj =>
+                           {
+                               Route phone = obj as Route;
+                               if (phone != null)
+                               {
+                                   Routes.Remove(phone);
+                               }
+                           },
+                           (obj) => Routes.Count > 0));
+            }
+        }
+    }
+}
---
+++ trpo-lw6/Route.cs
@@ -0,0 +1,107 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace trpo_lw6
+{
+    public class Route : INotifyPropertyChanged
+    {
+        private string _name;
+        private string _description;
+        private string _peak;
+        private int _height;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                _description = value;
+                OnPropertyChanged("Description");
+            }
+        }
+
+        public string Peak
+        {
+            get => _peak;
+            set
+            {
+                _peak = value;
+                OnPropertyChanged("Peak");
+            }
+        }
+
+        public int Height
+        {
+            get => _height;
+            set
+            {
+                _height = value;
+                OnPropertyChanged("Height");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
+
+//using System.ComponentModel;
+//using System.Runtime.CompilerServices;
+
+//namespace MVVM
+//{
+//    public class Phone : INotifyPropertyChanged
+//    {
+//        private string title;
+//        private string company;
+//        private int price;
+
+//        public string Title
+//        {
+//            get { return title; }
+//            set
+//            {
+//                title = value;
+//                OnPropertyChanged("Title");
+//            }
+//        }
+//        public string Company
+//        {
+//            get { return company; }
+//            set
+//            {
+//                company = value;
+//                OnPropertyChanged("Company");
+//            }
+//        }
+//        public int Price
+//        {
+//            get { return price; }
+//            set
+//            {
+//                price = value;
+//                OnPropertyChanged("Price");
+//            }
+//        }
+
+//        public event PropertyChangedEventHandler PropertyChanged;
+//        public void OnPropertyChanged([CallerMemberName] string prop = "")
+//        {
+//            if (PropertyChanged != null)
+//                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+//        }
+//    }
+//}
